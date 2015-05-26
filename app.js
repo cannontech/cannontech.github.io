7c91c3028263b5677f6388a2399e4cb785718193
erics-mac <erics-mac@eric-macs-MacBook-Pro.local>
2015-05-25 21:57:04 -0500

    more testing

---
+++ app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by erics-mac on 5/25/15.
+ */
+'use strict';
+
+var app = angular.module('midwest', []);
+
+app.module('MainController', '$scope', function($scope){
+
+  $scope.message = 'Midwest Software Solutions, Inc.';
+});
